de32c996d6aed50951d20a076763e723298f1bd4
Andrew Loeffler <aloeffler@gmail.com>
2021-03-01 18:06:04 -0500

    finish phase 2

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,4 +1,8 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   // Your JavaScript goes here...
-  
+  console.log("Hello World");
 });
+
+// window.onload(
+  // console.log("Hello World")
+// );
