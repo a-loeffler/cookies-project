d8541e03792b8b986d7e1bedc1bd2a15f677b866
kriskerby <kriskerby@gmail.com>
2021-03-01 15:20:13 -0800

    kris driving

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,19 +1,34 @@
-// window.addEventListener("DOMContentLoaded", (event) => {
-//   // Your JavaScript goes here...
-  // console.log("Hello World");
-// });
-
-
-window.onload(
-  console.log("Hello World");
-)
-
-
 window.addEventListener("DOMContentLoaded", (event) => {
   // Your JavaScript goes here...
   console.log("Hello World");
 });
 
+
 // window.onload(
-  // console.log("Hello World")
-// );
+  //   console.log("Hello World");
+  // )
+
+
+  // window.addEventListener("DOMContentLoaded", (event) => {
+    //   // Your JavaScript goes here...
+    //   console.log("Hello World");
+    // });
+
+    // // window.onload(
+      //   // console.log("Hello World")
+      // // );
+
+      // document.cookie = "monster_name=cookie";
+
+      document.cookie = "favorite_cookie=snickerdoodle";
+
+      document.cookie = "monster_name=cookie";
+
+      // window.alert("Hello World");
+
+      // window.prompt("Hello World");
+
+      function setCookie(name, value){
+        document.cookie = `${name}=${value}`;
+      }
+      setCookie("Monster", 200);
