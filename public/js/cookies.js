0574689d33823eb4155d4c81526e415171760eaf
Andrew Loeffler <aloeffler@gmail.com>
2021-03-01 18:51:20 -0500

    finish phase 5

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -9,26 +9,53 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // )
 
 
-  // window.addEventListener("DOMContentLoaded", (event) => {
-    //   // Your JavaScript goes here...
-    //   console.log("Hello World");
-    // });
+// window.addEventListener("DOMContentLoaded", (event) => {
+  //   // Your JavaScript goes here...
+  //   console.log("Hello World");
+  // });
 
-    // // window.onload(
-      //   // console.log("Hello World")
-      // // );
+// // window.onload(
+  //   // console.log("Hello World")
+  // // );
 
-      // document.cookie = "monster_name=cookie";
+  // document.cookie = "monster_name=cookie";
 
-      document.cookie = "favorite_cookie=snickerdoodle";
+document.cookie = "favorite_cookie=snickerdoodle";
 
-      document.cookie = "monster_name=cookie";
+document.cookie = "monster_name=cookie";
 
-      // window.alert("Hello World");
+// window.alert("Hello World");
 
-      // window.prompt("Hello World");
+// window.prompt("Hello World");
 
-      function setCookie(name, value){
-        document.cookie = `${name}=${value}`;
-      }
-      setCookie("Monster", 200);
+function setCookie(name, value){
+  document.cookie = `${name}=${value}`;
+}
+setCookie("Monster", 200);
+
+function getCookies() {
+  let cookieArray = document.cookie.split("; ");
+  return cookieArray;
+}
+//"favorite_cookie=snickerdoodle; monster_name=cookie, ...""
+console.log(getCookies());
+//["favorite_cookie=snickerdoodle", "monster_name=cookie", "Monster=200"]
+
+
+function getCookieValue(name) {
+  let cookieArray = getCookies();
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let ele = cookieArray[i];
+
+    let keyValueArray = ele.split("="); //makes an array
+
+    let key = keyValueArray[0];
+    let value = keyValueArray[1];
+    if (key === name) {
+      return value;
+    }
+  }
+  return null;
+
+}
